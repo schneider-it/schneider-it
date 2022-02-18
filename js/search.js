d36d721dfea1af7fe90b6ba86fa97808eb9f1058
Felix Schneider <f.schneider@htlkrems.at>
2022-02-18 22:18:52 +0100

    Search-Hover-Effect perfektionieren (wahrscheinlich)

--- js/search.js
+++ js/search.js
@@ -1,161 +1,160 @@
-const list = [
-    {
-      "title": "einfachen Datentypen",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Eigenschaften",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Eigenschaften",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Wertebereiche",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Wertebereiche",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Wertebereiche erklärt",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Wertebereiche_erklärt",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Operation mit Zahlen",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Operation_mit_Zahlen",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Int",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Int",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Double",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Double",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Float",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Float",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Bool",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Bool",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Struct",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Struct",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "short",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-short",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "long",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-long",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "byte",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-byte",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "sbyte",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-sbyte",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "uint",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-uint",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "ushort",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-ushort",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "ulong",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-ulong",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Eigenschaften",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Eigenschaften",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Char",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Char",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "String",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-String",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Array",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Array",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "List",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-List",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Class",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Class",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Enum",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Enum",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Object",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Object",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Delegate",
-      "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Delegate",
-      "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
-    },
-    {
-      "title": "Was ist das?",
-      "location": "/docs/programmiersprachen/brainfuck.html#Was_ist_das?",
-      "ordner": "Programmiersprachen -> Brainfuck",
-    },
-    {
-      "title": "Erklärung",
-      "location": "/docs/programmiersprachen/brainfuck.html#Erklärung",
-      "ordner": "Programmiersprachen -> Brainfuck",
-    },
-    {
-      "title": "Entwicklung",
-      "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Entwicklung",
-      "ordner": "Programmiersprachen -> Brainfuck",
-    },
-    {
-      "title": "Programmierung",
-      "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Programmierung",
-      "ordner": "Programmiersprachen -> Brainfuck",
-    },
-    {
-      "title": "Mit Brainfuck anfangen",
-      "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Mit_Brainfuck_anfangen",
-      "ordner": "Programmiersprachen -> Brainfuck",
-    }
-]
-
+// const list = [
+//     {
+//       "title": "einfachen Datentypen",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Eigenschaften",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Eigenschaften",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Wertebereiche",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Wertebereiche",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Wertebereiche erklärt",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Wertebereiche_erklärt",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Operation mit Zahlen",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Operation_mit_Zahlen",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Int",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Int",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Double",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Double",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Float",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Float",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Bool",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Bool",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Struct",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-Struct",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "short",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-short",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "long",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-long",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "byte",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-byte",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "sbyte",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-sbyte",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "uint",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-uint",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "ushort",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-ushort",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "ulong",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#einfache_Datentypen-ulong",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Eigenschaften",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Eigenschaften",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Char",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Char",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "String",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-String",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Array",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Array",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "List",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-List",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Class",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Class",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Enum",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Enum",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Object",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Object",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Delegate",
+//       "location": "/docs/programmiersprachen/csharp/grundlagen/datentypen.html#Referenztypen-Delegate",
+//       "ordner": "Programmiersprachen -> C# -> Grundlagen -> Datentypen",
+//     },
+//     {
+//       "title": "Was ist das?",
+//       "location": "/docs/programmiersprachen/brainfuck.html#Was_ist_das?",
+//       "ordner": "Programmiersprachen -> Brainfuck",
+//     },
+//     {
+//       "title": "Erklärung",
+//       "location": "/docs/programmiersprachen/brainfuck.html#Erklärung",
+//       "ordner": "Programmiersprachen -> Brainfuck",
+//     },
+//     {
+//       "title": "Entwicklung",
+//       "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Entwicklung",
+//       "ordner": "Programmiersprachen -> Brainfuck",
+//     },
+//     {
+//       "title": "Programmierung",
+//       "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Programmierung",
+//       "ordner": "Programmiersprachen -> Brainfuck",
+//     },
+//     {
+//       "title": "Mit Brainfuck anfangen",
+//       "location": "/docs/programmiersprachen/brainfuck.html#Erklärung-Mit_Brainfuck_anfangen",
+//       "ordner": "Programmiersprachen -> Brainfuck",
+//     }
+// ]
 const options = {
     isCaseSensitive: false,
     includeScore: false,
@@ -182,7 +181,16 @@ const options = {
     ]
 };
 
-const fuse = new Fuse(list, options);
+const fuse = null;
+fetch('./index.json').then(function (response) {
+  // The API call was successful!
+  return response.json()
+}).then(function (data){
+  fuse = new Fuse(data, options);
+}).catch(function (err) {
+  // There was an error
+  alert('Error in loading json file.');
+});
 
 $("#search").on('keyup', function (e) {
   if((e.key !== "Tab" || e.keyCode !== 9) && (e.key !== "Shift" || e.keyCode !== 16) && (e.key !== "Enter" || e.keyCode !== 13)) {
