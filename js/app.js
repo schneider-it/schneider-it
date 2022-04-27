6e271d0dfbcc96bfa77525fdbe70707cf84e966c
Programmierer281986 <f.schneider@htlkrems.at>
2022-04-27 16:01:43 +0200

    Impressum & Quellen + _grid.scss

--- js/app.js
+++ js/app.js
@@ -24,10 +24,10 @@ function OnScroll() {
     ScrollIndicator();
 }
 
-function OnKeyDown() {
-    e = window.event;
-    KeyCombination(e);
-}
+// function OnKeyDown() {
+//     e = window.event;
+//     KeyCombination(e);
+// }
 
 function VanillaTiltEffect() {
     VanillaTilt.init(document.querySelectorAll(".card"), {
@@ -155,56 +155,64 @@ function ShowCopyAccess(id) {
 }
 
 function LoadFooterandHeader() {
-    $("#header").load("/components/header.html", function () {
-        NavigationActiveToggle();
-        ThemeSwitch();
-        TastenkombinationenActiveToggle();
-        ActiveFollowCursor();
-    });
-    $("#footer").load("/components/footer.html", function () {
-        var myPix = new Array(
-            "/img/vogel1.png",
-            "/img/vogel2.png",
-            "/img/vogel3.png",
-            "/img/vogel4.gif",
-            "/img/vogel5.png",
-            "/img/vogel6.png",
-            "/img/vogel7.png",
-            "/img/vogel9.png",
-            "/img/vogel10.png",
-            "/img/vogel11.png",
-            "/img/vogel12.png",
-            "/img/vogel13.png",
-            "/img/vogel14.png",
-            "/img/vogel15.png",
-            "/img/vogel16.png",
-            "/img/vogel17.png",
-            "/img/vogel18.png",
-            "/img/vogel19.gif",
-            "/img/vogel20.gif",
-            "/img/vogel21.gif",
-            "/img/vogel22.gif",
-            "/img/vogel23.gif",
-            "/img/vogel24.gif",
-            "/img/vogel25.gif"
-        );
-        var randomNum = Math.floor(Math.random() * myPix.length);
-        try {
-            document.getElementById("vogel").src = myPix[randomNum];
-            if (Math.random() < 0.5 && myPix[randomNum] != "/img/vogel24.gif") {
-                document.getElementById("vogel").style.transform =
-                    "scale(-1, 1)";
-            }
-            // document.getElementById("vogel_a").style.cursor = "default";
+    if (document.getElementById("header") != null) {
+        console.log("inside");
+        $("#header").load("/components/header.html", function () {
+            NavigationActiveToggle();
+            ThemeSwitch();
+            TastenkombinationenActiveToggle();
+            ActiveFollowCursor();
+        });
+    }
+    if (document.getElementById("footer") != null) {
+        $("#footer").load("/components/footer.html", function () {
+            var myPix = new Array(
+                "/img/vogel1.png",
+                "/img/vogel2.png",
+                "/img/vogel3.png",
+                "/img/vogel4.gif",
+                "/img/vogel5.png",
+                "/img/vogel6.png",
+                "/img/vogel7.png",
+                "/img/vogel9.png",
+                "/img/vogel10.png",
+                "/img/vogel11.png",
+                "/img/vogel12.png",
+                "/img/vogel13.png",
+                "/img/vogel14.png",
+                "/img/vogel15.png",
+                "/img/vogel16.png",
+                "/img/vogel17.png",
+                "/img/vogel18.png",
+                "/img/vogel19.gif",
+                "/img/vogel20.gif",
+                "/img/vogel21.gif",
+                "/img/vogel22.gif",
+                "/img/vogel23.gif",
+                "/img/vogel24.gif",
+                "/img/vogel25.gif"
+            );
+            var randomNum = Math.floor(Math.random() * myPix.length);
+            try {
+                document.getElementById("vogel").src = myPix[randomNum];
+                if (
+                    Math.random() < 0.5 &&
+                    myPix[randomNum] != "/img/vogel24.gif"
+                ) {
+                    document.getElementById("vogel").style.transform =
+                        "scale(-1, 1)";
+                }
+                // document.getElementById("vogel_a").style.cursor = "default";
 
-            // if(Math.random() < .18){
-            //     document.getElementById("vogel_a").href = "34573r366.html";
-            //     document.getElementById("vogel_a").style.cursor = "pointer";
-            // }
-        } catch {
-            console.log("There are no birds in here!");
-        }
-    });
+                // if(Math.random() < .18){
+                //     document.getElementById("vogel_a").href = "34573r366.html";
+                //     document.getElementById("vogel_a").style.cursor = "pointer";
+                // }
+            } catch {
+                console.log("There are no birds in here!");
+            }
+        });
+    }
 }
 
 function ScrollIndicator() {
